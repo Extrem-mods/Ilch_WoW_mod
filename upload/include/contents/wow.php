c149a2976bdeb230914d44f1942da3689dfaa134
Kristian Hepper <KristianHepper@extrem-mods.de>
2011-07-10 01:00:21 +0200

    feinheiten

---
+++ upload/include/contents/wow.php
@@ -0,0 +1,10 @@
+<?php 
+
+defined ('main') or die ( 'no direct access' );
+
+switch($menu->get(1)) {
+	         case 'char':   $wowDatei = 'char';  break;
+	default: case 'realm':  $wowDatei = 'realm'; break;
+}
+ 
+require_once('include/contents/wow/'.$wowDatei.'.php');
