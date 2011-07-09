ef33fa4a28f26b88067f7631611455dd6ee1ca3d
Kristian Hepper <KristianHepper@extrem-mods.de>
2011-07-09 13:04:48 +0200

    einafch bischen was gemacht

--- upload/include/contents/installation.sql
+++ upload/include/contents/installation.sql
@@ -1,9 +1,10 @@
-CREATE TABLE `prefix_beispiel` (
-  `id` int(10) unsigned NOT NULL auto_increment,
-  `tid` smallint(6) NOT NULL default '0',
-  `autor` varchar(100) NOT NULL default '',
-  `pic` varchar(100) NOT NULL default '',
-  `homepage` varchar(100) NULL default '',
-  `aboutme` varchar(100) NOT NULL default '',
-  PRIMARY KEY  (`id`)
-)TYPE=MyISAM;
+CREATE TABLE `prefix_realms` (
+  `slug` varchar(15) NOT NULL,
+  `name` varchar(25) NOT NULL,
+  `type` enum('pvp', 'pve', 'rp') NOT NULL,
+  `queue` BOOL NOT NULL,
+  `status` BOOL NOT NULL,
+  `population` enum('low', 'medium', 'high') NOT NULL,
+  `refresh` TIMESTAMP NOT NULL, 
+  PRIMARY KEY  (`slug`)
+)TYPE=MyISAM COLLATE utf8_general_ci;
--- upload/include/includes/class/realm.php
+++ upload/include/includes/class/realm.php
@@ -1,4 +1,19 @@
 <?php
 
 class Realm{
+  private $_type; //typ (pve,pvp, rp)
+  private $_queue;  //Warteschlange (bool)
+  private $_status; //status (bool)
+  private $_population; 
+  private $_name;
+  private $_slug;
+
+  private function getDatasByDB(){
+  }
+  private function getDatasbyAPI(){
+  }
+  public function getDatas(){
+    if(!getDatasByDB()) return getDatasbyAPI();
+    return true; 
+  }
 }
