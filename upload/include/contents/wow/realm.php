68e65533fb933989e29faf6753bfbcf7fac0f929
Kristian Hepper <KristianHepper@extrem-mods.de>
2011-07-09 18:55:02 +0200

    fleissig am schreiben

--- upload/include/contents/wow/realm.php
+++ upload/include/contents/wow/realm.php
@@ -7,17 +7,19 @@ $design = new design ( $title , $hmenu, 1);
 $design->header();
 
 $tpl = new tpl ('wow/realmlist');
-
+$tpl->set_out(0);
 $realms = new Realmlist();
 $realms->getDatas();
 $slugs = $realms->getSlugs();
 foreach($slugs as $slug){
   $realm = $realms->getRealm($slug);
-  
-  $tpl->set_ar_out($realm->getAsArray(),1);
+  $out = $realm->getAsArray()
+  $out['status'] = (($out['status'])?'<span class="true">Online</span>':'<span class="false">Offline</span>')
+  $out['queue'] = (($out['queue'])?'<span class="true">Online</span>':'<span class="false">Offline</span>')
+    
+  $tpl->set_ar_out($out,1);
 }
-
-
+$tpl->set_out(2);
 }else{
 $slug= $menu->get(2) 
 }
